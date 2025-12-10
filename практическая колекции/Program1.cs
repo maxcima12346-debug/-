de0a3a08b8a4cbe4ae97c914b52be82b7e586515
maxcima12346-debug <maxcima12346@gmail.com>
2025-12-10 15:19:27 +0300

    Add files via upload

---
+++ практическая колекции/Program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> очередь = new Queue<string>();
+            очередь.Enqueue("Анна");
+            очередь.Enqueue("Пётр");
+            очередь.Enqueue("Ольга");
+            очередь.Enqueue("Иван");
+            очередь.Enqueue("Мария");
+            Console.Write("В очереди находятся: ");
+            Console.WriteLine(string.Join(", ", очередь.ToArray()));
+            while (очередь.Count > 0)
+            {
+                string элемент = очередь.Dequeue();
+                Console.WriteLine("Обслуживаем: " + элемент);
+            }
+            Console.WriteLine("Очередь закончилась");
+        }
+    }
+    
+}
---
+++ практическая колекции/Program2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            Console.WriteLine("Введите строки для добавления в стек (пустая строка для завершения):");
+            string input;
+            while (true) 
+            {
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) 
+                {
+                    break; 
+                }
+                stack.Push(input);
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Стек пуст.");
+            }
+            else
+            {
+                Console.WriteLine("Содержимое стека (от вершины к основанию):");
+                foreach (string item in stack.ToArray())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
